460e60cbcbc258d2067897a250d729f347982b0a
Jbalfour5 <jbalfour5@my.bcit.ca>
2024-11-05 01:06:58 -0800

    added mapbox to the map section once logged in

---
+++ scripts/mapbox.js
@@ -0,0 +1,42 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiamJhbGZvdXI1IiwiYSI6ImNtMnV0MnZxbzA1OTEya29iZG95NDgxaHgifQ.48MC1AtpUWyZww1hh6s7Iw';
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/light-v11',
+        center: [-77.04, 38.907],
+        zoom: 11.15,
+        attributionControl: false
+    });
+
+    
+    map.addControl(new mapboxgl.AttributionControl(), 'top-left');
+
+    const geolocate = new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true, 
+        showUserHeading: true
+    });
+     map.addControl(geolocate, 'right');
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const userCoordinates = [position.coords.longitude, position.coords.latitude];
+            map.flyTo({
+                center: userCoordinates,
+                zoom: 20, 
+                essential: true 
+            });
+        geolocate.trigger();
+        map.setCenter(userCoordinates); 
+        }, () => {
+            console.error('Error getting user location');
+            map.setCenter([-74.5, 40]);
+        });
+    } else {
+        console.error('Geolocation is not supported by this browser.');
+        map.setCenter([-74.5, 40]); 
+    }
+
+ 
+    
--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -5,19 +5,18 @@
 function loadSkeleton() {
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
-            // If the "user" variable is not null, then someone is logged in
-            console.log($('#navbarPlaceholder').load('./navbarAfterLogin.html', function() {
+            // User is signed in
+            $('#navbarPlaceholder').load('./navbarAfterLogin.html', function() {
                 activateNav();
-            }));
-            console.log($('#bottomNavPlaceholder').load('./bottomNavbar.html', function() {
+            });
+            $('#bottomNavPlaceholder').load('./bottomNavbar.html', function() {
                 activateNav();
-            }));
+            });
         } else {
-            // No user is signed in.
-            console.log($('#navbarPlaceholder').load('./navbarBeforeLogin.html', function() {
+            $('#navbarPlaceholder').load('./navbarBeforeLogin.html', function() {
                 activateNav();
-            }));
-            console.log($('#footerPlaceholder').load('./footer.html'));
+            });
+            $('#footerPlaceholder').load('./footer.html');
         }
     });
 }
