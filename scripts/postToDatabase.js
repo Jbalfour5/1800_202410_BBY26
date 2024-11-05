6205707995b64c00b29a0b62f0131f99af433f13
Jbalfour5 <jbalfour5@my.bcit.ca>
2024-11-05 09:07:59 -0800

    cleaned up bottom navbar, added profile.html

--- scripts/postToDatabase.js
+++ scripts/postToDatabase.js
@@ -1,3 +1,41 @@
+const selectImageButton = document.getElementById('selectImageButton');
+const imageInput = document.getElementById('imageInput');
+const selectedImage = document.getElementById('selectedImage');
+
+const targetWidth = 500; 
+const targetHeight = 300; 
+
+const postForm = document.getElementById('postForm');
+
+selectImageButton.addEventListener('click', function() {
+    imageInput.click();
+});
+
+imageInput.addEventListener('change', function(event) {
+    const file = event.target.files[0]; 
+    if (file) {
+        const reader = new FileReader(); 
+        reader.onload = function(e) {
+            const img = new Image(); 
+            img.src = e.target.result; 
+
+            img.onload = function() {
+                
+                const canvas = document.createElement('canvas');
+                const ctx = canvas.getContext('2d');
+
+                canvas.width = targetWidth;
+                canvas.height = targetHeight;
+
+                ctx.drawImage(img, 0, 0, targetWidth, targetHeight);
+
+                selectedImage.src = canvas.toDataURL('image/png');
+            };
+        };
+        reader.readAsDataURL(file); 
+    }
+});
+
 const submitPostButton = document.getElementById('submitPostButton');
 
 submitPostButton.addEventListener('click', () => {
@@ -11,14 +49,19 @@ submitPostButton.addEventListener('click', () => {
     const post = {
         title: postTitle,
         description: postDesc,
+        image: file,
         createdAt: new Date(),
     };
 
-    const db = firebase.firestore(); // Ensure firestore is initialized
+    const db = firebase.firestore(); 
     db.collection('posts').add(post)
     .then((docRef) => {
         console.log("Post written with ID: ", docRef.id);
-        // Optionally clear the form or show a success message
+        $('#submitPostButton').click(function() {
+            $('#postForm').addClass('hide');
+            $('#postForm').removeClass('show'); 
+        
+        });
     })
     .catch((error) => {
         console.error("Error adding post: ", error);
