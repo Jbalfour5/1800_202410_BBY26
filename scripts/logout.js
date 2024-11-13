5565ec124fb927d098fb804b75154df5467b5626
Jbalfour5 <jbalfour5@my.bcit.ca>
2024-11-13 01:29:56 -0800

    Made logout button functional

---
+++ scripts/logout.js
@@ -0,0 +1,9 @@
+function logout() {
+    firebase.auth().signOut().then(() => {
+      console.log("User signed out successfully.");
+      window.location.href = "index.html";
+    }).catch((error) => {
+      console.error("Error signing out:", error);
+    });
+  }
+  
